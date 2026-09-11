5b3014bb0c987b2aa6dbc8bbbe58393e95aca72d
Code And Pipe Smoke <codeandpipesmoke@gmail.com>
2026-09-11 11:42:05 +0200

    form jav

--- form.php
+++ form.php
@@ -2,118 +2,134 @@
                             <div class="col-md-10">
 
                                 <div class="card shadow" aria-labelledby="basic-form-title">
-                                    <div class="card-header border-bottom d-flex align-items-center justify-content-between">
-                                        <div>
-                                            <strong id="orders-title">Add New</strong>
-                                            <small class="d-block">Order item</small>
+                                    <form action="#" method="post" onsubmit="return false" enctype="multipart/form-data" class="form-horizontal">
+
+                                        <div class="card-header border-bottom d-flex align-items-center justify-content-between">
+                                            <div>
+                                                <strong id="orders-title">Add New</strong>
+                                                <small class="d-block">Order item</small>
+                                            </div>
+                                            <a href="#" class="m-btn m-btn--ghost" style="padding: 0 10px;" aria-label="Bezárás" title="Bezárás">
+                                                <i class="fa-solid fa-xmark" aria-hidden="true"></i>
+                                            </a>
                                         </div>
-                                        <a href="#" class="m-btn m-btn--ghost" style="padding: 0 10px;" aria-label="Bezárás" title="Bezárás">
-                                            <i class="fa-solid fa-xmark" aria-hidden="true"></i>
-                                        </a>
-                                    </div>
-									
-                                    <div class="card-body card-body">
-                                        <form action="#" method="post" onsubmit="return false" enctype="multipart/form-data" class="form-horizontal">
-                                            <div class="row mb-3">
-                                                <div class="col col-md-2 text-start text-md-end">
-                                                    <label for="text-input" class="form-control-label fw-bold">Text Input:</label>
+                                        
+                                        <div class="card-body card-body">
+                                            <form action="#" method="post" onsubmit="return false" enctype="multipart/form-data" class="form-horizontal">
+                                                <div class="row mb-3">
+                                                    <div class="col col-md-2 text-start text-md-end">
+                                                        <label for="text-input" class="form-control-label fw-bold">Text Input:</label>
+                                                    </div>
+                                                    <div class="col-12 col-md-9">
+                                                        <input type="text" id="text-input" name="text-input" placeholder="Text" class="is-invalid form-control" autofocus>
+                                                    </div>
+                                                    <div class="col-12 col-md-12 offset-md-2">
+                                                        <small class="form-text fw-bold text-danger">This is an error text</small>
+                                                    </div>
                                                 </div>
-                                                <div class="col-12 col-md-9">
-                                                    <input type="text" id="text-input" name="text-input" placeholder="Text" class="is-invalid form-control" autofocus>
+                                                
+                                                
+                                                <div class="row mb-3 align-items-start">
+                                                    <div class="col col-md-2 text-start text-md-end">
+                                                        <label for="textarea-input" class="form-control-label fw-bold">Textarea:</label>
+                                                    </div>
+                                                    <div class="col-12 col-md-9">
+                                                        <textarea name="textarea-input" id="textarea-input" rows="9" placeholder="Content..." class="form-control"></textarea>
+                                                    </div>
                                                 </div>
-												<div class="col-12 col-md-12 offset-md-2">
-													<small class="form-text fw-bold text-danger">This is an error text</small>
-												</div>
-                                            </div>
-											
-											
-                                            <div class="row mb-3 align-items-start">
-                                            <div class="col col-md-2 text-start text-md-end">
-                                                    <label for="textarea-input" class="form-control-label fw-bold">Textarea:</label>
+                                                
+                                                
+                                                <div class="row mb-3">
+                                                    <div class="col col-md-2 text-start text-md-end">
+                                                        <label for="select" class="form-control-label fw-bold">Select:</label>
+                                                    </div>
+                                                    <div class="col-12 col-md-9">
+                                                        <select name="select" id="select" class="form-select">
+                                                            <option value="0">Please select</option>
+                                                            <option value="1">Option #1</option>
+                                                            <option value="2">Option #2</option>
+                                                            <option value="3">Option #3</option>
+                                                        </select>
+                                                    </div>
                                                 </div>
-                                                <div class="col-12 col-md-9">
-                                                    <textarea name="textarea-input" id="textarea-input" rows="9" placeholder="Content..." class="form-control"></textarea>
+                                                
+
+                                                <div class="row mb-3">
+                                                    <div class="col col-md-2 text-start text-md-end">
+                                                        <label for="selectLg" class="form-control-label fw-bold">Select Large:</label>
+                                                    </div>
+                                                    <div class="col-12 col-md-9">
+                                                        <select name="selectLg" id="selectLg" class="form-select form-select-lg">
+                                                            <option value="0">Please select</option>
+                                                            <option value="1">Option #1</option>
+                                                            <option value="2">Option #2</option>
+                                                            <option value="3">Option #3</option>
+                                                        </select>
+                                                    </div>
                                                 </div>
-                                            </div>
-											
-											
-											
-                                            <div class="row mb-3">
-                                                <div class="col col-md-2 text-start text-md-end">
-                                                    <label for="select" class="form-control-label fw-bold">Select:</label>
+                                                
+
+                                                <div class="row mb-3">
+                                                    <div class="col col-md-2 text-start text-md-end">
+                                                        <label for="selectSm" class="form-control-label fw-bold">Select Small:</label>
+                                                    </div>
+                                                    <div class="col-12 col-md-9">
+                                                        <select name="selectSm" id="SelectLm" class="form-select form-select-sm">
+                                                            <option value="0">Please select</option>
+                                                            <option value="1">Option #1</option>
+                                                            <option value="2">Option #2</option>
+                                                            <option value="3">Option #3</option>
+                                                            <option value="4">Option #4</option>
+                                                            <option value="5">Option #5</option>
+                                                        </select>
+                                                    </div>
                                                 </div>
-                                                <div class="col-12 col-md-9">
-                                                    <select name="select" id="select" class="form-select">
-                                                        <option value="0">Please select</option>
-                                                        <option value="1">Option #1</option>
-                                                        <option value="2">Option #2</option>
-                                                        <option value="3">Option #3</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-											
-                                            <div class="row mb-3">
-                                                <div class="col col-md-2 text-start text-md-end">
-                                                    <label for="selectLg" class="form-control-label fw-bold">Select Large:</label>
-                                                </div>
-                                                <div class="col-12 col-md-9">
-                                                    <select name="selectLg" id="selectLg" class="form-select form-select-lg">
-                                                        <option value="0">Please select</option>
-                                                        <option value="1">Option #1</option>
-                                                        <option value="2">Option #2</option>
-                                                        <option value="3">Option #3</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-											
-                                            <div class="row mb-3">
-                                                <div class="col col-md-2 text-start text-md-end">
-                                                    <label for="selectSm" class="form-control-label fw-bold">Select Small:</label>
-                                                </div>
-                                                <div class="col-12 col-md-9">
-                                                    <select name="selectSm" id="SelectLm" class="form-select form-select-sm">
-                                                        <option value="0">Please select</option>
-                                                        <option value="1">Option #1</option>
-                                                        <option value="2">Option #2</option>
-                                                        <option value="3">Option #3</option>
-                                                        <option value="4">Option #4</option>
-                                                        <option value="5">Option #5</option>
-                                                    </select>
-                                                </div>
-                                            </div>
 
 
-                                            <div class="row mb-3 align-items-center">
-                                                <div class="col col-md-2 text-start text-md-end">
-                                                    <label class="form-control-label fw-bold mb-0">Inline Checkboxes:</label>
+                                                <div class="row mb-3 align-items-center">
+                                                    <div class="col col-md-2 text-start text-md-end">
+                                                        <label class="form-control-label fw-bold mb-0">Inline Checkboxes:</label>
+                                                    </div>
+                                                    <div class="col col-md-9 d-flex align-items-center flex-wrap pt-1">
+                                                        <div class="form-check form-check-inline">
+                                                            <input type="checkbox" id="inline-checkbox1" name="inline-checkbox1" value="option1" class="form-check-input">
+                                                            <label for="inline-checkbox1" class="form-check-label">One</label>
+                                                        </div>
+                                                        <div class="form-check form-check-inline">
+                                                            <input type="checkbox" id="inline-checkbox2" name="inline-checkbox2" value="option2" class="form-check-input">
+                                                            <label for="inline-checkbox2" class="form-check-label">Two</label>
+                                                        </div>
+                                                        <div class="form-check form-check-inline">
+                                                            <input type="checkbox" id="inline-checkbox3" name="inline-checkbox3" value="option3" class="form-check-input">
+                                                            <label for="inline-checkbox3" class="form-check-label">Three</label>
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                                <div class="col col-md-9 d-flex align-items-center flex-wrap pt-1">
-                                                    <div class="form-check form-check-inline">
-                                                        <input type="checkbox" id="inline-checkbox1" name="inline-checkbox1" value="option1" class="form-check-input">
-                                                        <label for="inline-checkbox1" class="form-check-label">One</label>
+
+                                                <div class="row mb-3">
+                                                    <div class="col col-md-2 text-start text-md-end">
+                                                        <label for="file-input" class="form-control-label fw-bold">File Input:</label>
                                                     </div>
-                                                    <div class="form-check form-check-inline">
-                                                        <input type="checkbox" id="inline-checkbox2" name="inline-checkbox2" value="option2" class="form-check-input">
-                                                        <label for="inline-checkbox2" class="form-check-label">Two</label>
-                                                    </div>
-                                                    <div class="form-check form-check-inline">
-                                                        <input type="checkbox" id="inline-checkbox3" name="inline-checkbox3" value="option3" class="form-check-input">
-                                                        <label for="inline-checkbox3" class="form-check-label">Three</label>
+                                                    <div class="col-12 col-md-9">
+                                                        <input type="file" id="file-input" name="file-input" class="form-control">
                                                     </div>
                                                 </div>
+                                            </form>
+                                        </div>
+                                        
+
+                                        <div class="card-footer border-top">
+                                            <div class="offset-md-2">
+                                                <button type="submit" class="btn btn-success">
+                                                    <i class="fa-regular fa-dot-circle"></i> Save
+                                                </button>
+                                                <button type="reset" class="btn btn-secondary">
+                                                    <i class="fa-solid fa-ban"></i> Cancel
+                                                </button>
                                             </div>
-                                        </form>
-                                    </div>
-                                    <div class="card-footer border-top">
-										<div class="offset-md-2">
-											<button type="submit" class="btn btn-success">
-												<i class="fa-regular fa-dot-circle"></i> Save
-											</button>
-											<button type="reset" class="btn btn-secondary">
-												<i class="fa-solid fa-ban"></i> Cancel
-											</button>
-										</div>
-                                    </div>
+                                        </div>
+
+                                    </form>
                                 </div>
                             </div>
                         </div>
